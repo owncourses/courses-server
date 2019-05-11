177a279c736a99aa8cbb101aaec236628988fc3c
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2019-05-11 11:09:41 +0200

    add new properties to course admin

--- src/Admin/CourseAdmin.php
+++ src/Admin/CourseAdmin.php
@@ -6,6 +6,7 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -16,6 +17,27 @@ final class CourseAdmin extends AbstractAdmin
         $container = $this->getConfigurationPool()->getContainer();
         $formMapper->add('title', TextType::class);
         $formMapper->add('description', TextType::class, ['required' => false]);
+        $formMapper->add('visible', null, ['required' => false]);
+        $formMapper->add('startDate', DateTimeType::class, [
+            'widget' => 'single_text',
+            'format' => DateTimeType::HTML5_FORMAT,
+            'required' => false,
+            'html5' => true,
+            'input' => 'datetime',
+            'attr' => [
+                'style' => 'width: 200px',
+            ],
+        ]);
+        $formMapper->add('endDate', DateTimeType::class, [
+            'widget' => 'single_text',
+            'format' => DateTimeType::HTML5_FORMAT,
+            'required' => false,
+            'html5' => true,
+            'input' => 'datetime',
+            'attr' => [
+                'style' => 'width: 200px',
+            ],
+        ]);
 
         $fileFieldOptions = ['required' => false];
         if (null !== $this->getSubject()->getCoverImageName()) {
@@ -36,5 +58,8 @@ final class CourseAdmin extends AbstractAdmin
         $listMapper->addIdentifier('title');
         $listMapper->add('description');
         $listMapper->add('coverImageName');
+        $listMapper->add('visible');
+        $listMapper->add('startDate');
+        $listMapper->add('endDate');
     }
 }
