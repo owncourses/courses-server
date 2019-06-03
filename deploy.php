886d291142f5dc5533f23bb62616054e3b87fb41
Paweł Mikołajczuk <pawel@mikolajczuk.in>
2019-06-03 06:42:43 +0200

    add ckeeditor assets install command to deployer, dump .env cached php file after deployment, add jwt token ttl value to env

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/symfony4.php';
 set('application', 'owncourses');
 set('default_stage', 'demo');
 
-set('env', function () {
+set('env', static function () {
     return [
         'APP_ENV' => has('symfony_env') ? get('symfony_env') : 'prod',
     ];
@@ -23,6 +23,17 @@ set('shared_files', [
 ]); // reset sharing .env
 
 inventory('hosts.yml');
+
+task('deploy:assets:install', static function () {
+    run('{{bin/console}} ckeditor:install');
+    run('{{bin/console}} assets:install {{release_path}}/public');
+})->desc('Install bundle assets');
+after('deploy:symlink', 'deploy:assets:install');
+
+task('deploy:composer:dump-env', static function () {
+    run('cd {{release_path}} && {{bin/composer}} symfony:dump-env prod');
+})->desc('Install bundle assets');
+after('deploy:symlink', 'deploy:composer:dump-env');
 
 after('deploy:failed', 'deploy:unlock');
 before('deploy:symlink', 'database:migrate');
