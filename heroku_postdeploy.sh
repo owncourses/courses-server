30a461b0a5fef3b3e8f4241620b7aea55d7fbb3c
Paweł Mikołajczuk <pawel@mikolajczuk.in>
2019-06-09 21:10:02 +0200

    disable questions on fixtures load command (#25)

--- heroku_postdeploy.sh
+++ heroku_postdeploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-/app/.heroku/php/bin/php bin/console doctrine:fixtures:load &&
+/app/.heroku/php/bin/php bin/console doctrine:fixtures:load --no-interaction &&
 /app/.heroku/php/bin/php bin/console app:user:create test@example.com testPassword Super Test --courses="From courses maker ZERO to HERO" &&
 /app/.heroku/php/bin/php bin/console app:user:promote test@example.com ROLE_ADMIN
