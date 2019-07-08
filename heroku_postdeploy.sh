5da0d768355b74d9b7679db93850399e7bdb48b5
Paweł Mikołajczuk <pawel@mikolajczuk.in>
2019-07-08 19:58:28 +0200

    add assets:install to post install heroku tasks

--- heroku_postdeploy.sh
+++ heroku_postdeploy.sh
@@ -2,3 +2,4 @@
 /app/.heroku/php/bin/php bin/console doctrine:fixtures:load --no-interaction &&
 /app/.heroku/php/bin/php bin/console app:user:create test@example.com testPassword Super Test --courses="From courses maker ZERO to HERO" &&
 /app/.heroku/php/bin/php bin/console app:user:promote test@example.com ROLE_ADMIN
+/app/.heroku/php/bin/php bin/console assets:install
