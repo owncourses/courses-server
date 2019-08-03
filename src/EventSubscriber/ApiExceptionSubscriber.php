af6bfbfe381c8102bc0b5aee25dcd2ba17ff9d2c
Paweł Mikołajczuk <pawel@mikolajczuk.in>
2019-08-03 10:33:04 +0200

    improve excepition status code handling

--- src/EventSubscriber/ApiExceptionSubscriber.php
+++ src/EventSubscriber/ApiExceptionSubscriber.php
@@ -2,8 +2,10 @@
 
 namespace App\EventSubscriber;
 
+use SWP\Component\Common\Exception\HttpException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
 class ApiExceptionSubscriber implements EventSubscriberInterface
@@ -11,12 +13,17 @@ class ApiExceptionSubscriber implements EventSubscriberInterface
     public function onExceptionEvent(ExceptionEvent $event)
     {
         $request = $event->getRequest();
+        $exception = $event->getException();
         if ('application/json' === $request->headers->get('Content-Type')) {
-            $event->allowCustomResponseCode();
+            $code = Response::HTTP_INTERNAL_SERVER_ERROR;
+            if ($exception instanceof HttpException) {
+                $code = $exception->getStatusCode();
+            }
+
             $event->setResponse(new JsonResponse([
                 'success' => false,
-                'message' => $event->getException()->getMessage(),
-            ], $event->getException()->getStatusCode()));
+                'message' => $exception->getMessage(),
+            ], $code));
         }
     }
 
